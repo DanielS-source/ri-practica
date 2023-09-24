4b90ab8efc16c62bff3c479bb82aa5821179ec01
DanielS-source <daniel.silva.iglesias@udc.es>
2023-09-24 14:15:44 +0200

    Fixed settings for scrappy and elastic

--- metacritic/metacritic/settings.py
+++ metacritic/metacritic/settings.py
@@ -99,7 +99,7 @@ ITEM_PIPELINES = {
     'scrapyelasticsearch.scrapyelasticsearch.ElasticSearchPipeline': 500
 }
 
-ELASTICSEARCH_SERVERS = ['localhost']
+ELASTICSEARCH_SERVERS = ['http://localhost:9200/']
 ELASTICSEARCH_INDEX = 'scrapy'
 #ELASTICSEARCH_INDEX_DATE_FORMAT = '%Y-%m'
 ELASTICSEARCH_TYPE = 'items'
